893a41e6bd52c8c63152a522e9be89e899236575
Andrey Sennikov <99654050+SennikovAndrey@users.noreply.github.com>
2023-05-08 00:10:10 +0300

    Update dynamic_programming.py

--- knapsack/dynamic_programming.py
+++ knapsack/dynamic_programming.py
@@ -10,12 +10,14 @@ def findAns(weights, table, k, s):
 
 def dynamic_programming(W, cost, weights):
     n = len(cost)
+    count_operations = 0
     global ans_alg
     ans_alg = [0 for i in range(0, n)]
     table = [[0 for x in range(W + 1)] for x in range(n + 1)]
 
     for i in range(1, n + 1):
         for j in range(1, W + 1):
+            count_operations += 1
             if weights[i - 1] <= j:
                 table[i][j] = max(cost[i - 1] + table[i - 1][j - weights[i - 1]], table[i - 1][j])
             else:
@@ -27,4 +29,4 @@ def dynamic_programming(W, cost, weights):
     for i in range(n):
         if ans_alg[i] == 1:
             sum_weight += weights[i]
-    return ans_alg, table[n][W], sum_weight, 0
+    return ans_alg, table[n][W], sum_weight, count_operations
